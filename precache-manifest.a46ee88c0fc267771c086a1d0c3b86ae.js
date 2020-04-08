203f8d7dc3c04c9a564b0534338097c3eaa4ff89
Julio Cesar Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-04-08 12:09:42 -0500

    upload production

---
+++ precache-manifest.a46ee88c0fc267771c086a1d0c3b86ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d34e76c72c628496cf14585b591173",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "504c6828e71829dc8480",
+    "url": "/covid19/static/css/main.d03bfe86.chunk.css"
+  },
+  {
+    "revision": "630936b35a9417dbbaf1",
+    "url": "/covid19/static/js/2.d38bf36f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.d38bf36f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504c6828e71829dc8480",
+    "url": "/covid19/static/js/main.1a26899f.chunk.js"
+  },
+  {
+    "revision": "fb0281db9800ab5b0326",
+    "url": "/covid19/static/js/runtime-main.29310ac9.js"
+  },
+  {
+    "revision": "7bfa8915bfca0b4bfefd6d0a410e1021",
+    "url": "/covid19/static/media/image.7bfa8915.png"
+  }
+]);
